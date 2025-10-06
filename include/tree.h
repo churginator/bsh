churginator/bsh

commit 6cf9ae9cce7f9daf35cfdd66550c36fbd1885f3e

"temporary" fix

src/cmd.c
@@ -128,11 +128,13 @@ int load_tree(tree_t *input) {
     node_t *root;
     int execute_ret;
     token_t parent_type;
+    int dont_care;
 
     root = input->root;
     execute_ret = 0;
     leftmost = next_leaf(root);
     next = NULL;
+    dont_care = 0;
 
     if (leftmost == NULL) {
         // The input has to have at least one node, so we assume root == leftmost
@@ -160,10 +162,15 @@ int load_tree(tree_t *input) {
 
         next = next_leaf((node_t *) leftmost);
 
-        if (next == NULL) {
+        if (next == NULL || dont_care == 1) {
             execute_ret = execute_normal(leftmost);
             kill_leaf(leftmost);
-            break;
+            if (next == NULL) {
+                break;
+            } else {
+                leftmost = next;
+                continue;
+            }
         }
 
         switch (parent_type) {
@@ -189,6 +196,7 @@ int load_tree(tree_t *input) {
         kill_leaf(leftmost);
         leftmost = next;
         if (execute_ret == -1) break;
+        dont_care++;
     }
 
     if (sigprocmask(SIG_UNBLOCK, &blocker, NULL) != 0) {

include/safe_mallocs.h
@@ -1,7 +1,7 @@
-#include <stddef.h>
-
 #ifndef SAFE_MALLOCS_H
 #define SAFE_MALLOCS_H
+
+#include <stddef.h>
 
 void *safe_malloc(size_t size);
 void *safe_realloc(void *ptr, size_t size);

commit ec11ef7b26e620b9985c99cfcaae2d35dea89184

every codebase degrades with time
mine just degrades especially quickly

fixed the bugs 6cf9ae9 introduced, replaced strtok, escape characters wip

include/tree.h
@@ -33,8 +33,9 @@ typedef struct {
 node_t *create_node(node_t *parent);
 leaf_t *create_leaf(node_t *parent);
 int kill_leaf(leaf_t *who);
+node_t *next_node(node_t *start);
 leaf_t *next_leaf(node_t *start);
-tree_t *init_tree();
+tree_t *init_tree(node_t *root);
 void delete_tree(tree_t *who);
 
 #endif // TREE_H

src/cmd.c
@@ -124,17 +124,17 @@ static int execute_pipe(leaf_t *cmd) {
 
 int load_tree(tree_t *input) {
     leaf_t *leftmost;
-    leaf_t *next;
+    leaf_t *next_l;
+    node_t *next_n;
     node_t *root;
     int execute_ret;
     token_t parent_type;
-    int dont_care;
 
     root = input->root;
     execute_ret = 0;
     leftmost = next_leaf(root);
-    next = NULL;
-    dont_care = 0;
+    next_l = NULL;
+    next_n = NULL;
 
     if (leftmost == NULL) {
         // The input has to have at least one node, so we assume root == leftmost
@@ -155,32 +155,30 @@ int load_tree(tree_t *input) {
     }
 
     while (leftmost != NULL) {
+        next_n = next_node((node_t *) leftmost);
+        next_l = NULL; // otherwise calling next_leaf() causes a segfault
+
         // if we don't do this, calling next_leaf() may cause a use-after-free
-        if (leftmost->meta.parent != NULL) {
-            parent_type = leftmost->meta.parent->type;
+        if (next_n != NULL) {
+            parent_type = next_n->type;
+            next_l = next_leaf(next_n);
         }
 
-        next = next_leaf((node_t *) leftmost);
-
-        if (next == NULL || dont_care == 1) {
+        // done traversing
+        if (next_l == NULL) {
             execute_ret = execute_normal(leftmost);
             kill_leaf(leftmost);
-            if (next == NULL) {
-                break;
-            } else {
-                leftmost = next;
-                continue;
-            }
+            break;
         }
 
         switch (parent_type) {
         case REDIRECTION:
-            execute_ret = execute_indir(leftmost, next);
-            next = next_leaf((node_t *) leftmost);
+            execute_ret = execute_indir(leftmost, next_l);
+            next_l = next_leaf((node_t *) leftmost);
             break;
         case PIPE:
             leftmost->stdout = execute_pipe(leftmost);
-            next->stdin = leftmost->stdout;
+            next_l->stdin = leftmost->stdout;
             execute_ret = leftmost->stdout;
             break;
         case SEMICOLON:
@@ -194,9 +192,8 @@ int load_tree(tree_t *input) {
         }
 
         kill_leaf(leftmost);
-        leftmost = next;
+        leftmost = next_l;
         if (execute_ret == -1) break;
-        dont_care++;
     }
 
     if (sigprocmask(SIG_UNBLOCK, &blocker, NULL) != 0) {

src/lexer.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <lexer.h>
+#include <stdio.h>
 
 lexer_t *create_lexer() {
     return safe_malloc(sizeof(lexer_t));
@@ -64,7 +65,13 @@ static node_t *lex_internal(lexer_t *lexer, node_t *parent) {
     do {
         n = next_char(lexer);
         symbol_found = true;
-
+/*
+        if (next_char(lexer) == '\\') {
+            continue;
+        } else {
+            reverse(lexer);
+        }
+*/
         switch (n) {
         case '>':
             next = create_node(parent);

src/path.c
@@ -10,37 +10,44 @@
 char **path_entries;
 
 char **tokenize(char *input, char **output, char delim) {
-
-	size_t delim_count = 0;
 	size_t output_size;
-	char delim_string[2];
-	char *ret;
+    size_t output_idx;
+	char *previous;
 
-	delim_string[0] = delim;
-	delim_string[1] = '\0';
+    output_idx = 0;
+    output_size = 16 * sizeof(char *);
+    output = safe_realloc(output, output_size);
+    previous = input;
 
 	for (size_t i = 0; input[i] != '\0'; i++) {
-		if (input[i] == delim)
-			delim_count++;
+        if ((output_idx >= output_size)) {
+            output_size += 16 * sizeof(char *);
+            output = safe_realloc(output, output_size);
+        }
+
+        if (input[i] == '\\') {
+            i++;
+            continue;
+        } else if (input[i] == delim) {
+            input[i] = '\0';
+            do {
+                i++;
+            } while (input[i] == delim && input[i] != '\0');
+
+            output[output_idx] = previous;
+            output_idx++;
+            previous = input + i;
+        }
 	}
 
-	output_size = (delim_count + 2) * sizeof(char *);
+    output[output_idx] = previous;
+    output_idx++;
+    output[output_idx] = NULL;
 
-	output = safe_realloc(output, output_size);
-
-	ret = strtok(input, delim_string);
-
-	for (size_t i = 0; ret != NULL; i++) {
-		output[i] = ret;
-		ret = strtok(NULL, delim_string);
-	}
-
-	output[delim_count + 1] = NULL;
 	return output;
 }
 
 char **init_path(char **output) {
-
 	char *path_envvar;
 	size_t path_envvar_size;
 	char *s;
@@ -59,7 +66,6 @@ char **init_path(char **output) {
 }
 
 int handle_path(char *input, char *output, char **paths) {
-
 	if (strchr(input, '/') != NULL) {
 		strcpy(output, input);
 		return 0;

src/tree.c
@@ -91,6 +91,18 @@ static node_t *traverse(node_t *start) {
     return output;
 }
 
+node_t *next_node(node_t *start) {
+    node_t *curr;
+
+    curr = start;
+    
+    do {
+        curr = traverse(curr);
+    } while (curr != NULL && curr->meta.left == NULL && curr->meta.right == NULL);
+
+    return curr;
+}
+
 leaf_t *next_leaf(node_t *start) {
     node_t *curr;
 