0d6229e8d05c1904cd9e98761dc46d846b070e1f
absolute idiot <174307533+churginator@users.noreply.github.com>
2025-10-28 18:21:10 -0400

    woops

--- src/builtins.c
+++ src/builtins.c
@@ -1,12 +1,21 @@
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <error.h>
 
 #include <builtins.h>
 
+// returns 0 if a builtin was found, 1 otherwise
+// the return status of the builtin is placed in n
 int handle_builtins(char **input, int *n) {
     if (!strcmp(input[0], "cd")) {
-        chdir(input[1]);
-        *n = 0;
+        if (chdir(input[1]) == -1) {
+            error(EXIT_SUCCESS, errno, "%s", input[1]);
+            *n = 1;
+        } else {
+            *n = 0;
+        }
     } else {
         return 0;
     }
