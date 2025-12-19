3654f07e649cfa94902e17c68801ee041a70559a
absolute idiot <174307533+churginator@users.noreply.github.com>
2025-12-18 22:40:24 -0500

    added exit, style fix

--- src/builtins.c
+++ src/builtins.c
@@ -8,14 +8,17 @@
 
 // returns 0 if a builtin was found, 1 otherwise
 // the return status of the builtin is placed in n
-int handle_builtins(char **input, int *n) {
-    if (!strcmp(input[0], "cd")) {
-        if (chdir(input[1]) == -1) {
-            error(EXIT_SUCCESS, errno, "%s", input[1]);
+int handle_builtins(char **argv, int *n) {
+    if (!strcmp(argv[0], "cd")) {
+        if (chdir(argv[1]) == -1) {
+            error(EXIT_SUCCESS, errno, "%s", argv[1]);
             *n = 1;
         } else {
             *n = 0;
         }
+    } else if (!strcmp(argv[0], "exit")) {
+        // if this returns somehow im killing myself
+        exit(0);
     } else {
         return 0;
     }
